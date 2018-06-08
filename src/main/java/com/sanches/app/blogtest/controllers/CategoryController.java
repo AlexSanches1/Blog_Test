1562be3c4839ef8a8c220b62d5e3603e06f88cb1
Sanches <sanches2026@gmail.com>
2018-06-08 11:37:35 +0300

    */Modified classes:    - CategoryController;    - PostController;    and their services; */Added delete functionality: Categories and Posts

--- src/main/java/com/sanches/app/blogtest/controllers/CategoryController.java
+++ src/main/java/com/sanches/app/blogtest/controllers/CategoryController.java
@@ -1,20 +1,25 @@
 package com.sanches.app.blogtest.controllers;
 
+import com.sanches.app.blogtest.models.dto.RequestCategoryDTO;
+import com.sanches.app.blogtest.repositories.CategoryRepository;
 import com.sanches.app.blogtest.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/category")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @PostMapping("/create")
-    public ResponseEntity createCategoryByName(@RequestParam("name") String name) {
-        return ResponseEntity.ok(categoryService.createCategory(name));
+    public ResponseEntity createCategoryByName(@RequestBody RequestCategoryDTO dto) {
+        return ResponseEntity.ok(categoryService.createCategory(dto.getName()));
     }
 
     @GetMapping("/list")
@@ -25,5 +30,10 @@ public class CategoryController {
     @GetMapping("/byId")
     public ResponseEntity getById(@RequestParam("id") Long id) {
         return ResponseEntity.ok(categoryService.getCategory(id));
+    }
+
+    @GetMapping("/delete_category/byId")
+    public void deleteById(Long id) {
+        categoryRepository.deleteById(id);
     }
 }
